4520bf549aaa8762b412cfee1a7b04834722a9ca
김진영 <skswls0@daum.net>
2022-11-25 20:36:30 +0900

    서버와 클라이언트 간 websocket 연결
    Co-Authored-By: Matajeu <116011087+matajeu@users.noreply.github.com>

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,2 @@
-console.log('hello');
+const socket = new WebSocket(`ws://${window.location.host}`); // 연결된 server를 의미함
+console.log(socket);
--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+import http from 'http'; // nodejs 내장
+import WebSocket from 'ws';
 import express from 'express';
 
 const app = express();
@@ -6,8 +8,18 @@ app.set('view engine', 'pug');
 app.set('views', __dirname + '/views');
 app.use('/public', express.static(__dirname + '/public'));
 
-app.get('/', (req, res) => res.render('home'));
+// routing
+app.get('/', (_, res) => res.render('home'));
+app.get('/*', (_, res) => res.redirect('/'));
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 
-app.listen(3000, handleListen);
+const server = http.createServer(app); // server access 가능
+
+const ws = new WebSocket.Server({ server });
+
+ws.on('connection', socket => {
+   console.log(socket);
+});
+
+server.listen(3000, handleListen);
