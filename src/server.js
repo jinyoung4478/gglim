5f4586b8f134e93622bedea03f2ca140601cb293
김진영 <skswls0@daum.net>
2022-12-01 00:37:24 +0900

    채팅방 생성 및 초기 세팅
    Co-Authored-By: Matajeu <116011087+matajeu@users.noreply.github.com>

--- src/server.js
+++ src/server.js
@@ -13,53 +13,12 @@ app.get('/*', (_, res) => res.redirect('/'));
 const httpServer = http.createServer(app);
 const wsServer = SocketIO(httpServer);
 
-function publicRooms() {
-   const {
-      sockets: {
-         adapter: { sids, rooms },
-      },
-   } = wsServer;
-   // public room list
-   const publicRooms = [];
-   rooms.forEach((_, key) => {
-      if (sids.get(key) === undefined) {
-         publicRooms.push(key);
-      }
-   });
-   return publicRooms;
-}
-
-function countRoom(roomName) {
-   return wsServer.sockets.adapter.rooms.get(roomName)?.size;
-}
-
 wsServer.on('connection', socket => {
-   socket['nickname'] = 'Anon';
-   socket.onAny(event => {
-      console.log(`Socket Event: ${event}`);
-   });
-   socket.on('enter_room', (roomName, done) => {
+   socket.on('joinRoom', (roomName, done) => {
       socket.join(roomName);
-      done(countRoom(roomName));
-      socket.to(roomName).emit('welcome', socket.nickname, countRoom(roomName)); // send message everybody except for me
-      // 모든 소켓에 메세지 보내기
-      wsServer.sockets.emit('room_change', publicRooms());
-   });
-   socket.on('disconnecting', () => {
-      socket.rooms.forEach(room => socket.to(room).emit('bye', socket.nickname, countRoom(room) - 1));
-      // disconnecting event는 socket이 방을 떠나기 직전에 실행되는 것
-      // 따라서 이 emit은 disconnecting 전에 일어남
-      // 완전 연결 종료 이후 event를 발생시키려면 disconnect를 사용
-      //wsServer.sockets.emit('room_change', publicRooms());
-   });
-   socket.on('disconnect', () => {
-      wsServer.sockets.emit('room_change', publicRooms());
-   });
-   socket.on('newMessage', (msg, room, done) => {
-      socket.to(room).emit('newMessage', `${socket.nickname}: ${msg}`);
       done();
+      socket.to(roomName).emit('welcome');
    });
-   socket.on('nickname', nickname => (socket['nickname'] = nickname));
 });
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
