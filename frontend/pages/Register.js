e4e846460e0f7c556fa56b33a810bd44330e3829
김진영 <skswls0@daum.net>
2022-12-10 02:46:06 +0900

    feat: 코드 정리
    Co-Authored-By: Matajeu <116011087+matajeu@users.noreply.github.com>

--- frontend/pages/Register.js
+++ frontend/pages/Register.js
@@ -19,11 +19,13 @@ const Register = {
                         id="register-email"
                         placeholder="이메일 계정"
                      />
-                     <button id="register-email-button" class="register-input__button">
+                     <button id="register-email-button" class="register-input__button" disabled>
                         인증하기
                      </button>
                   </div>
-                  <p id="register-email-message" class="register-validate-message">이메일 정보를 입력해 주세요</p>
+                  <p id="register-email-message" class="register-validate-message">
+                     이메일 정보를 입력해 주세요
+                  </p>
                   <div id="register-email-confirm" class="register-form__wrapper-email">
                      <div class="register-form__wrapper-email">
                         <input
@@ -34,9 +36,13 @@ const Register = {
                            placeholder="인증번호 입력"
                         />
                         <span id="register-timer"></span>
-                        <button id="register-email-code-button" class="register-input__button">확인</button>
+                        <button id="register-email-code-button" class="register-input__button" disabled>
+                           확인
+                        </button>
                      </div>
-                     <p id="register-email-code-message" class="register-validate-message">인증번호 6자리를 입력하세요.</p>
+                     <p id="register-email-code-message" class="register-validate-message">
+                        인증번호 6자리를 입력하세요.
+                     </p>
                   </div>
                </div>
                <div class="register-form__wrapper">
@@ -83,16 +89,27 @@ const Register = {
       const emailCodeMessage = document.querySelector('#register-email-code-message');
       let timer = null;
 
-      emailConfirmWrapper.style.display = 'none';
-      emailMessage.style.display = 'none';
-      emailButton.setAttribute('disabled', '');
+      initAllElements();
+      addAllEvents();
 
-      //validateEmail;
-      emailInput.addEventListener('input', handleInputEmail);
-      emailCodeInput.addEventListener('input', handleInputEmailCode);
-      emailButton.addEventListener('click', handleEmailAuthButton);
+      function initAllElements() {
+         emailConfirmWrapper.style.display = 'none';
+         emailMessage.style.display = 'none';
+         //emailButton.setAttribute('disabled', '');
+      }
 
-      function handleInputEmail(e) {
+      function addAllEvents() {
+         // Email Input
+         emailInput.addEventListener('input', handleEmailInput);
+         // Email Auth Button
+         emailButton.addEventListener('click', handleEmailButton);
+         // Email Code Input
+         emailCodeInput.addEventListener('input', handleEmailCodeInput);
+         // Email Code Check Button
+         emailCodeButton.addEventListener('click', handleEmailCodeButton);
+      }
+
+      function handleEmailInput(e) {
          e.preventDefault();
          const email = e.target.value;
          const isValide = validateEmail(email);
@@ -108,25 +125,7 @@ const Register = {
          }
       }
 
-      function handleInputEmailCode(e) {
-         e.preventDefault();
-         const code = e.target.value;
-         // 이메일 인증 코드 6자리 형식 자동 포맷
-         emailCodeInput.value = formatEmailCode(code);
-         // 이메일 인증 코드 유효성 검증
-         const isValid = validateEmailCode(code);
-         if (isValid) {
-            emailCodeInput.style['border-color'] = null;
-            emailCodeMessage.style.color = null;
-            emailCodeMessage.innerText = '인증번호 6자리를 입력하세요.';
-         } else {
-            emailCodeInput.style['border-color'] = '#f66';
-            emailCodeMessage.style.color = '#f66';
-            emailCodeMessage.innerText = '인증번호를 정확히 입력해 주세요.';
-         }
-      }
-
-      async function handleEmailAuthButton(e) {
+      async function handleEmailButton(e) {
          e.preventDefault();
          // Show email authentication code form
          emailButton.innerText = '재전송';
@@ -177,6 +176,37 @@ const Register = {
                emailCodeMessage.innerText = '인증 유효 시간을 초과했습니다. 다시 시도해 주세요.';
             }
          }, 1000);
+      }
+
+      function handleEmailCodeInput(e) {
+         e.preventDefault();
+         const code = e.target.value;
+         // 이메일 인증 코드 6자리 형식 자동 포맷
+         emailCodeInput.value = formatEmailCode(code);
+         // 이메일 인증 코드 유효성 검증
+         const isValid = validateEmailCode(code);
+         if (isValid) {
+            emailCodeInput.style['border-color'] = null;
+            emailCodeMessage.style.color = null;
+            emailCodeMessage.innerText = '인증번호 6자리를 입력하세요.';
+            emailCodeButton.removeAttribute('disabled');
+         } else {
+            emailCodeInput.style['border-color'] = '#f66';
+            emailCodeMessage.style.color = '#f66';
+            emailCodeMessage.innerText = '인증번호를 정확히 입력해 주세요.';
+            emailCodeButton.setAttribute('disabled', '');
+         }
+      }
+
+      async function handleEmailCodeButton(e) {
+         e.preventDefault();
+         // 최종 Email Code 유효성 검사
+         const code = emailCodeInput.value;
+         // 이메일 인증 코드 유효성 검증
+         const isValid = validateEmailCode(code);
+         if (isValid) {
+            console.log('Req', code);
+         }
       }
    },
 };
