bd10f3e873468d36ee7cfd9360b84f97c4b0640f
김진영 <skswls0@daum.net>
2022-12-12 02:04:04 +0900

    feat: auth 기능 분리
    Co-Authored-By: Matajeu <116011087+matajeu@users.noreply.github.com>

---
+++ backend/middlewares/login-required.js
@@ -0,0 +1,39 @@
+import jwt from 'jsonwebtoken';
+
+function loginRequired(req, res, next) {
+   const userToken = req.headers['authorization']?.split(' ')[1];
+   if (!userToken || userToken === 'null') {
+      console.log('서비스 사용 요청이 있습니다.하지만, Authorization 토큰: 없음');
+      const error = new Error('로그인한 유저만 사용할 수 있는 서비스입니다.');
+      error.status = 403;
+      throw error;
+   }
+   try {
+      const secretKey = process.env.JWT_SECRET_KEY || 'secret-key';
+      const jwtDecoded = jwt.verify(userToken, secretKey);
+
+      const userId = jwtDecoded.userId;
+      const role = jwtDecoded.role;
+
+      req.currentUserId = userId;
+      req.currentRole = role;
+
+      next();
+   } catch (err) {
+      if (err.name === 'TokenExpiredError') {
+         // 유효기간 초과
+         console.log('토큰 기간 만료');
+         const error = new Error('토큰이 만료되었습니다.');
+         error.status = 401;
+         throw error;
+         // front에서 해야할 처리
+         // removeItem token
+         // sessionStorage.removeItem('token');
+      }
+      const error = new Error('유효하지 않은 토큰입니다.');
+      error.status = 401;
+      throw error;
+   }
+}
+
+export { loginRequired };
